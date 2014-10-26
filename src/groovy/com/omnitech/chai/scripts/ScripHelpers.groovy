2ad596a4d5c8bf0a238e1d5d4741852029eac83d
kayr <noryak1234@gmail.com>
2014-10-26 22:04:40 +0300

    More helper segmentation methods

--- src/groovy/com/omnitech/chai/scripts/ScripHelpers.groovy
+++ src/groovy/com/omnitech/chai/scripts/ScripHelpers.groovy
@@ -10,30 +10,39 @@ class ScripHelpers {
 
     static int getProductCount(String products) {
         def m = ['less than 10': 5, '10-30': 20, 'more than 30': 35]
-        return m[products]
+        return m[products] ?: 0
     }
 
 
     static int getTurnOver(String products) {
         def map = ['less than 50,000 UGX': 2500, '50,000-150,000 UGX': 100000, '150,000 - 300,000 UGX': 235000, 'greater than 300,000 UGX': 350000]
-        return map[products]
+        return map[products] ?: 0
     }
 
 
-    static def calcScore(Customer c, double weight, Map<Object, Closure<Number>> functions) {
+    static def calcScore(Customer c, double weight, def functions) {
+        if (functions instanceof Closure) {
+            return (functions(c) * weight)
+        }
         def func = functions.get(c.outletType)
         return (func(c) as Float) * weight
     }
 
 
-    static int getRangeScore(def value, List ranges) {
+    static int intRangeScore(def value, List ranges) {
         def size = ranges.size()
         for (int i = 0; i < size; i++) {
             def val = ranges[i]
             if (i == 0 && value > val) return size
-            if (value >= val) return (size == i+1) ? 1 : size - 1
+            if (value >= val) return (size == i + 1) ? 1 : size - 1
         }
-        return 1
+        return 0
+    }
+
+    static int objRangeScore(def value, List ranges) {
+        def score = ranges.indexOf(value)
+        if (score != -1) return score
+        return 0
     }
 
 
--- test/unit/com/omnitech/chai/scripts/ScripHelpersTest.groovy
+++ test/unit/com/omnitech/chai/scripts/ScripHelpersTest.groovy
@@ -2,7 +2,7 @@ package com.omnitech.chai.scripts
 
 import spock.lang.Specification
 
-import static com.omnitech.chai.scripts.ScripHelpers.getRangeScore
+import static com.omnitech.chai.scripts.ScripHelpers.intRangeScore
 
 /**
  * Created by kay on 10/26/14.
@@ -11,17 +11,17 @@ class ScripHelpersTest extends Specification {
     def "GetRangeScore"() {
 
         when:
-        def val = getRangeScore(30, [50, 15, 0])
+        def val = intRangeScore(30, [50, 15, 0])
         then:
         val == 2
 
         when:
-        val = getRangeScore(60, [50, 15, 0])
+        val = intRangeScore(60, [50, 15, 0])
         then:
         val == 3
 
         when:
-        val = getRangeScore(7, [50, 15, 0])
+        val = intRangeScore(7, [50, 15, 0])
         then:
         val == 1
     }
