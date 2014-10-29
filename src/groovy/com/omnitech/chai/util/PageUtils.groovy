2cb4fb265a4bc5e34596a7b5ae1c7112cb0e0a99
kayr <noryak1234@gmail.com>
2014-10-29 21:52:04 +0300

    Cypher query paginator

--- src/groovy/com/omnitech/chai/util/PageUtils.groovy
+++ src/groovy/com/omnitech/chai/util/PageUtils.groovy
@@ -1,7 +1,12 @@
 package com.omnitech.chai.util
 
+import org.neo4j.cypherdsl.Order
+import org.neo4j.cypherdsl.grammar.ReturnNext
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
+
+import static org.neo4j.cypherdsl.CypherQuery.identifier
+import static org.neo4j.cypherdsl.CypherQuery.order
 
 /**
  * Created by kay on 9/24/14.
@@ -40,6 +45,16 @@ class PageUtils {
         if (sort)
             return new PageRequest(pageNumber, size, direction, sort)
         return new PageRequest(pageNumber, size)
+    }
+
+    static ReturnNext addPagination(ReturnNext next, Map param, Class returnClass) {
+        def entityName = returnClass.simpleName.toLowerCase()
+        def pageParams = create(param)
+        def sort = pageParams.sort?.toString()?.replace(':', '')
+        next.orderBy(order(identifier(entityName).property(sort ? sort : 'id'), Order.ASCENDING))
+                .skip(pageParams.offset)
+                .limit(pageParams.pageSize)
+        return next
     }
 
 }
