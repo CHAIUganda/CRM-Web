24b5fa8edf10298eb7fd3f6904c491d0278b5f33
kayr <noryak1234@gmail.com>
2014-10-29 13:59:01 +0300

    remove pagination on places controller

--- grails-app/controllers/com/omnitech/chai/rest/PlaceController.groovy
+++ grails-app/controllers/com/omnitech/chai/rest/PlaceController.groovy
@@ -1,9 +1,6 @@
 package com.omnitech.chai.rest
 
-import com.omnitech.chai.model.Region
-import grails.transaction.Transactional
-
-import static org.springframework.http.HttpStatus.*
+import static org.springframework.http.HttpStatus.OK
 
 /**
  * Created by kay on 10/29/14.
@@ -16,72 +13,20 @@ class PlaceController {
     def regionService
 
     //id,name,uuid
-    def regions(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
+    def regions() {
         def regions = regionService.listAllRegions().collect { [id: it.id, name: it.name, uuid: it.uuid] }
         respond regions, [status: OK]
     }
 
     //id,name,uuid
-    def districts(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
+    def districts() {
         def districts = regionService.listAllDistricts().collect { [id: it.id, name: it.name, uuid: it.uuid] }
         respond districts, [status: OK]
     }
 
     //id,name,uuid
-    def subCounties(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
+    def subCounties() {
         def subCounties = regionService.listAllSubCountys().collect { [id: it.id, name: it.name, uuid: it.uuid] }
         respond subCounties, [status: OK]
     }
-
-
-    @Transactional
-    def save(Region region) {
-        if (region == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        region.validate()
-        if (region.hasErrors()) {
-            render status: NOT_ACCEPTABLE
-            return
-        }
-
-        region.save flush: true
-        respond region, [status: CREATED]
-    }
-
-    @Transactional
-    def update(Region region) {
-        if (region == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        region.validate()
-        if (region.hasErrors()) {
-            render status: NOT_ACCEPTABLE
-            return
-        }
-
-        region.save flush: true
-        respond region, [status: OK]
-    }
-
-    @Transactional
-    def delete(Region region) {
-
-        if (region == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        region.delete flush: true
-        render status: NO_CONTENT
-    }
-
-
 }
