f1727fa1c3dc04e9ab09bc4038084a64fdfa0ca4
kayr <noryak1234@gmail.com>
2014-10-30 21:36:45 +0300

    Forgot to commit the Task Rest Contoller

---
+++ grails-app/controllers/com/omnitech/chai/rest/TaskController.groovy
@@ -0,0 +1,27 @@
+package com.omnitech.chai.rest
+
+import com.omnitech.chai.model.User
+import com.omnitech.chai.util.ReflectFunctions
+
+/**
+ * Created by kay on 10/29/14.
+ */
+class TaskController {
+
+    static namespace = 'rest'
+    static responseFormats = ['json', 'xml']
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+
+    def taskService
+    def neoSecurityService
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        def user = neoSecurityService.currentUser as User
+        def tasks = taskService.findAllTaskForUser(user.id)
+        def taskMaps = tasks.collect { ReflectFunctions.extractProperties(it) }
+        respond taskMaps
+    }
+
+}
