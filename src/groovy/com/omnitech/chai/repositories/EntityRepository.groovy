d15aa66ccdf2c2a98f28d67d0a55fda167642b91
kayr <noryak1234@gmail.com>
2014-11-05 11:56:01 +0300

    Remove hardcoded user id in task query

--- src/groovy/com/omnitech/chai/repositories/EntityRepository.groovy
+++ src/groovy/com/omnitech/chai/repositories/EntityRepository.groovy
@@ -59,7 +59,7 @@ interface TaskRepository extends GraphRepository<Task> {
     Task findLastTask(@Param('customerId') Long customerId)
 
     //START u=node(1) MATCH (u)-[:USER_TERRITORY]->(t)<-[:SC_IN_TERRITORY]-(sc)<-[:BELONGS_TO_SC]-(sc)-[:HAS_PARISH]->(p)-[:HAS_VILLAGE]->(v)<-[:CUST_IN_VILLAGE]-(customer)-[:CUST_TASK]-(tsk) RETURN distinct tsk
-    @Query('START u=node(1) MATCH (u)-[:USER_TERRITORY]->(t)<-[:SC_IN_TERRITORY]-(sc)-[:HAS_PARISH]->(p)-[:HAS_VILLAGE]->(v)<-[:CUST_IN_VILLAGE]-(customer)-[:CUST_TASK]-(tsk) WHERE u-[:ASSIGNED_TASK]->(tsk) or NOT(tsk<-[:ASSIGNED_TASK]-()) RETURN distinct tsk')
+    @Query('START u=node({userId}) MATCH (u)-[:USER_TERRITORY]->(t)<-[:SC_IN_TERRITORY]-(sc)-[:HAS_PARISH]->(p)-[:HAS_VILLAGE]->(v)<-[:CUST_IN_VILLAGE]-(customer)-[:CUST_TASK]-(tsk) WHERE u-[:ASSIGNED_TASK]->(tsk) or NOT(tsk<-[:ASSIGNED_TASK]-()) RETURN distinct tsk')
     Iterable<Task> findAllTaskForUser(@Param("userId") Long userId)
 
 }
