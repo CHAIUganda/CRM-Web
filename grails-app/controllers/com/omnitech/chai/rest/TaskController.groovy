c8ae17d4d14d0e86aea2c901ce986c125a2537d6
kayr <noryak1234@gmail.com>
2014-11-05 11:55:07 +0300

    add missing customer id on task rest interface

--- grails-app/controllers/com/omnitech/chai/rest/TaskController.groovy
+++ grails-app/controllers/com/omnitech/chai/rest/TaskController.groovy
@@ -20,7 +20,12 @@ class TaskController {
         params.max = Math.min(max ?: 10, 100)
         def user = neoSecurityService.currentUser as User
         def tasks = taskService.findAllTaskForUser(user.id)
-        def taskMaps = tasks.collect { ReflectFunctions.extractProperties(it) }
+        def taskMaps = tasks.collect {
+            def map = ReflectFunctions.extractProperties(it)
+            map['customerId'] = it.customer.id
+            map
+        }
+
         respond taskMaps
     }
 
