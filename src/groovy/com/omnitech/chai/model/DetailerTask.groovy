8d6dab25831fc0536c564c48e7f3ba1fd53bd0bd
kayr <noryak1234@gmail.com>
2014-11-07 19:08:57 +0300

    Forgot to commit this

---
+++ src/groovy/com/omnitech/chai/model/DetailerTask.groovy
@@ -0,0 +1,37 @@
+package com.omnitech.chai.model
+
+import org.springframework.data.neo4j.annotation.NodeEntity
+
+/**
+ * Created by kay on 11/7/14.
+ */
+@NodeEntity
+class DetailerTask extends Task {
+
+
+    Integer diarrheaPatientsInFacility
+    String heardAboutDiarrheaTreatmentInChildren
+    String howDidYouHear
+    String otherWaysHowYouHeard
+    String whatYouKnowAbtDiarrhea
+    String diarrheaEffectsOnBody
+    String knowledgeAbtOrsAndUsage
+    String knowledgeAbtZincAndUsage
+    String whyNotUseAntibiotics
+    Boolean doYouStockOrsZinc
+    Integer howManyZincInStock
+    Integer howmanyOrsInStock
+    String zincBrandsold
+    String orsBrandSold
+    String ifNoWhy
+    Double zincPrice
+    Double orsPrice
+    Double buyingPrice
+    String pointOfsaleMaterial
+    String recommendationNextStep
+    String recommendationLevel
+
+    def beforeSave() {
+        this.type = Order.simpleName
+    }
+}
