b7d98c9087baabd58d9b379f77a02e1d42bc118d
kayr <noryak1234@gmail.com>
2014-11-13 13:54:53 +0300

    Add unique constraints for all Model objects

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,8 +2,11 @@ import com.omnitech.chai.model.RequestMap
 import com.omnitech.chai.model.Role
 import com.omnitech.chai.model.User
 import com.omnitech.chai.util.ChaiUtils
-import com.omnitech.chai.util.ModelFunctions
 import grails.plugin.springsecurity.ReflectionUtils
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider
+import org.springframework.core.type.filter.AnnotationTypeFilter
+import org.springframework.data.neo4j.annotation.NodeEntity
 
 class BootStrap {
 
@@ -39,6 +42,8 @@ class BootStrap {
             }
         }
 
+        createUuidConstraints()
+
         //override this so that a proper request map is loaded by spring security
         ReflectionUtils.metaClass.static.getRequestMapClass = { RequestMap }
     }
@@ -46,36 +51,22 @@ class BootStrap {
         graphDatabaseService.shutdown()
     }
 
-    def initdata() {
-
-        def superAdminRole = Role.findByAuthority('ROLE_SUPER_ADMIN') ?: new Role(authority: 'ROLE_SUPER_ADMIN').save(failOnError: true)
-        def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
-
-        def superAdminUser = User.findByUsername('super') ?: new User(
-                username: 'super',
-                password: 'pass',
-                enabled: true).save(failOnError: true)
-
-        def adminUser = User.findByUsername('root') ?: new User(
-                username: 'root',
-                password: 'pass',
-                enabled: true).save(failOnError: true)
-
-        if (!superAdminUser.authorities.contains(superAdminRole)) {
-            UserRole.create superAdminUser, superAdminRole
+    def createUuidConstraints() {
+        txHelperService.doInTransaction {
+            getPersistentEntities().each {
+                def constrainQuery = "CREATE CONSTRAINT ON (bean:${getSimpleName(it.beanClassName)}) ASSERT bean.uuid IS UNIQUE"
+                println("Creating Unique UUID Constraint for $it.beanClassName")
+                neo.query(constrainQuery, [:])
+            }
         }
-
-        if (!adminUser.authorities.contains(adminRole)) {
-            UserRole.create adminUser, adminRole
-        }
-
-
-        for (String url in [
-                '/login/auth', '/**/js/**', '/**/css/**',
-                '/**/images/**', '/**/favicon.ico']) {
-            new RequestMap(url: url, configAttribute: 'permitAll').save()
-        }
-        new RequestMap(url: '/**', configAttribute: 'ROLE_SUPER_ADMIN,ROLE_ADMIN').save()
-//        new RequestMap(url: '/**', configAttribute: 'ROLE_ADMIN').save()
     }
+
+    String getSimpleName(String className) { Class.forName(className).simpleName }
+
+    Set<BeanDefinition> getPersistentEntities() {
+        def provider = new ClassPathScanningCandidateComponentProvider(false);
+        provider.addIncludeFilter(new AnnotationTypeFilter(NodeEntity));
+        provider.findCandidateComponents("com.omnitech.chai.model");
+    }
+
 }
