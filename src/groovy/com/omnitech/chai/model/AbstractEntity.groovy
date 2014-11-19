5aeaf7d3594d0b9688f7bfeeb2c1dd74755589ee
kayr <noryak1234@gmail.com>
2014-11-19 14:29:21 +0300

    Add readable date strings

--- src/groovy/com/omnitech/chai/model/AbstractEntity.groovy
+++ src/groovy/com/omnitech/chai/model/AbstractEntity.groovy
@@ -2,14 +2,38 @@ package com.omnitech.chai.model
 
 import org.springframework.data.neo4j.annotation.GraphId
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+
 /**
  * Created by kay on 9/28/14.
  */
 
 class AbstractEntity {
+    private static DateFormat format = new SimpleDateFormat('yyyy-MM-dd hh:mm:ss')
     @GraphId
     Long id
     String uuid
     Date dateCreated
     Date lastUpdated
+    String _dateCreated
+    String _dateLastUpdated
+
+    Date getDateCreated() {
+        return dateCreated
+    }
+
+    void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated
+        this._dateCreated = format.format(dateCreated)
+    }
+
+    Date getLastUpdated() {
+        return lastUpdated
+    }
+
+    void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated
+        this._dateLastUpdated = format.format(lastUpdated)
+    }
 }
