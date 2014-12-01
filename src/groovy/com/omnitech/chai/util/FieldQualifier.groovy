042dab5d99b99dbd473f6b7fce0ec4b4c400f4ed
kayr <noryak1234@gmail.com>
2014-12-01 23:04:13 +0300

    Is allowed pattern matcher

--- src/groovy/com/omnitech/chai/util/FieldQualifier.groovy
+++ src/groovy/com/omnitech/chai/util/FieldQualifier.groovy
@@ -14,27 +14,51 @@ class FieldQualifier {
         this.filters = filters
     }
 
-    boolean isAllowed(Class klass, String fieldName) {
-        if(checkDenied(klass,fieldName)){
+    boolean isAllowed(String className, String fieldName) {
+        if (!filters) return true
+
+        if (checkIsDenied(className, fieldName)) {
             return false
         }
 
-
+        return checkIsAllowed(className, fieldName)
     }
 
-    private boolean checkAllowed(Class klass, String fieldName){
+    private boolean checkIsAllowed(String klass, String fieldName) {
         def allAllowedEntries = filters.allow
-        def deniedEntry = allAllowedEntries?.find { stringsMatch(it['class'].toString(), klass.simpleName) }
+
+        //if there are no whiteList entries then all are allowed
+        if (!allAllowedEntries) {
+            return true
+        }
+
+        def allowedEntry = allAllowedEntries?.find { stringsMatch(it['class'].toString(), klass) }
+
+        // if there are no entries for this class then this field is not allowed
+        if (!allowedEntry) {
+            return false
+        }
+
+        def patterns = allowedEntry.patterns as List<String>
+
+        //if there are no patterns for this class then this field allowed
+        if (!patterns) {
+            return true
+        }
+
+        return patterns.any { stringsMatch(it, fieldName) }
+
 
     }
 
-    private boolean checkDenied(Class klass, String fieldName) {
+    private boolean checkIsDenied(String klass, String fieldName) {
         def allDeniedEntries = filters.deny
-        def deniedEntry = allDeniedEntries?.find { stringsMatch(it['class'].toString(), klass.simpleName) }
+        def deniedEntry = allDeniedEntries?.find { stringsMatch(it['class'].toString(), klass) }
+
+        if (!deniedEntry) return false
 
         //if there are no patterns then all fields are denied
         def patterns = deniedEntry.patterns as List<String>
-
         if (!patterns) {
             return true
         }
@@ -46,5 +70,13 @@ class FieldQualifier {
     private static boolean stringsMatch(String pattern, String right) {
         def patternRegex = pattern.replace('*', '.*')
         return right.matches(patternRegex)
+    }
+
+    static boolean isAllowed(Map pattern, Class klass, String fieldName) {
+        isAllowed(pattern, klass.simpleName, fieldName)
+    }
+
+    static boolean isAllowed(Map pattern, String klass, String fieldName) {
+        return new FieldQualifier(pattern).isAllowed(klass, fieldName)
     }
 }
