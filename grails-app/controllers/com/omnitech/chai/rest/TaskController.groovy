2950327078f4b0f8b6cb8e8fce1361aca3eb3156
kayr <noryak1234@gmail.com>
2014-12-29 20:01:45 +0300

    detailers get Datailing tasks and sales people get orders

--- grails-app/controllers/com/omnitech/chai/rest/TaskController.groovy
+++ grails-app/controllers/com/omnitech/chai/rest/TaskController.groovy
@@ -1,6 +1,7 @@
 package com.omnitech.chai.rest
 
 import com.omnitech.chai.model.DetailerTask
+import com.omnitech.chai.model.Order
 import com.omnitech.chai.model.Task
 import com.omnitech.chai.model.User
 import com.omnitech.chai.util.ChaiUtils
@@ -9,8 +10,8 @@ import com.omnitech.chai.util.ReflectFunctions
 import grails.converters.JSON
 import org.springframework.http.HttpStatus
 
-import static com.omnitech.chai.model.Role.getDETAILER_ROLE_NAME
-import static com.omnitech.chai.model.Role.getSALES_ROLE_NAME
+import static com.omnitech.chai.model.Role.DETAILER_ROLE_NAME
+import static com.omnitech.chai.model.Role.SALES_ROLE_NAME
 
 /**
  * Created by kay on 10/29/14.
@@ -29,10 +30,30 @@ class TaskController {
         params.max = Math.min(max ?: 10, 2000)
         def user = neoSecurityService.currentUser as User
         log.debug("Req:${user}  - TaskList: $params")
-        def tasks = taskService.findAllTasksForUser(user.id, Task.STATUS_NEW, params,Task)
+        Class<Task> taskType
+        if (user.hasRole(DETAILER_ROLE_NAME))
+            taskType = DetailerTask
+        else if (user.hasRole(SALES_ROLE_NAME))
+            taskType = Order
+        else {
+            log.warn("user: [$user] has no detailer or sales role.... sending empty list")
+            respond([])
+            return
+        }
+
+        def tasks = taskService.findAllTasksForUser(user.id, Task.STATUS_NEW, params, taskType)
         def taskMaps = tasks.collect {
             def map = ReflectFunctions.extractProperties(it)
             map['customerId'] = it.customer.id
+            if (Order.isAssignableFrom(it.getClass())) {
+                map['lineItems'] = ((Order) it).lineItems.collect {
+                    ['productId': it.product.uuid,
+                     'quantity' : it.quantity,
+                     'unitPrice': it.unitPrice
+                    ]
+
+                }
+            }
             return map
         }
         log.debug("Resp:${user} - ${taskMaps?.size()} Tasks...")
@@ -47,7 +68,7 @@ class TaskController {
         } else if (user.hasRole(SALES_ROLE_NAME)) {
             doOrderUpdate(user)
         } else {
-            renderError("You Do No Have Appropriate Roles To Perform This Task",HttpStatus.FORBIDDEN)
+            renderError("You Do No Have Appropriate Roles To Perform This Task", HttpStatus.FORBIDDEN)
         }
     }
 
