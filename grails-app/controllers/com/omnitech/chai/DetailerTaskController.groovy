33008cf61dbf671958eac3337bd409312e756322
kayr <noryak1234@gmail.com>
2015-02-08 17:24:50 +0300

    Refactoring detailer task controller

--- grails-app/controllers/com/omnitech/chai/DetailerTaskController.groovy
+++ grails-app/controllers/com/omnitech/chai/DetailerTaskController.groovy
@@ -1,219 +1,56 @@
 package com.omnitech.chai
 
+import com.omnitech.chai.model.Customer
 import com.omnitech.chai.model.DetailerTask
-import com.omnitech.chai.model.Task
 import com.omnitech.chai.util.ModelFunctions
-import com.omnitech.chai.util.ReflectFunctions
-import com.omnitech.chai.util.ServletUtil
-import fuzzycsv.FuzzyCSV
-import grails.converters.JSON
-import grails.transaction.Transactional
-import grails.util.GrailsNameUtils
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.neo4j.support.Neo4jTemplate
-import org.springframework.security.access.AccessDeniedException
-
-import static com.omnitech.chai.util.ControllerUtils.customerToJsonMap
-import static com.omnitech.chai.util.ControllerUtils.taskToJsonMap
-import static com.omnitech.chai.util.ModelFunctions.extractId
-import static org.springframework.http.HttpStatus.*
 
 /**
  * DetailerTaskController
  * A controller class handles incoming web requests and performs actions such as redirects, rendering views and so on.
  */
-class DetailerTaskController extends BaseController {
+class DetailerTaskController extends TaskController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def taskService
-    def userService
-    def regionService
-    def customerService
-    def txHelperService
-    def neoSecurityService
-    @Autowired
-    Neo4jTemplate neo
 
     def index(Integer max) {
-        def user = neoSecurityService.currentUser
-        if (params.remove('ui') == 'map') {
-            redirect(action: 'map', params: params)
-            return
-        }
-        def (page, users) = taskService.loadPageDataForUser(user, DetailerTask, params, max, null)
-        render(view: '/task/index', model: [taskInstanceList: page.content, taskInstanceCount: page.totalElements, users: users])
+        super.index max, DetailerTask, [view: '/task/index']
     }
 
     def map(Integer max) {
-
-        def user = neoSecurityService.currentUser
-        def (page, users) = taskService.loadPageDataForUser(user, DetailerTask, params, max, null)
-        def taskData = page.content.collect { Task task ->
-            return taskToJsonMap(task)
-        }
-
-
-        if (params.user) {
-            def userInContext = userService.findUserByName(params.user)
-            //todo use a query for this section to improve performance
-            def customers = customerService.findAllCustomersByUser(userInContext.id, true, [max: 2000])
-            customers.removeAll { c ->
-                page.any { it?.customer?.id == c.id }
-            }
-            def customerData = customers.findAll { it.wkt != null }.collect { customerToJsonMap(it) }
-            taskData.addAll(customerData)
-        }
-
-        def mapData = taskData as JSON
-
-        def jsonMapString = mapData.toString(true)
-        render(view: '/task/map', model: [taskInstanceList: page.content, taskInstanceCount: page.totalElements, users: users, mapData: jsonMapString])
+        super.map max, DetailerTask, [view: '/task/map']
     }
 
     def export() {
-        def user = params.user ? userService.findUserByName(params.user) : null
-        def exportFields = ['DISTRICT', 'SUBCOUNTY', 'VILLAGE', 'OUTLET NAME', 'OUTLET TYPE']
-        def fields = ReflectFunctions.findAllBasicFields(DetailerTask).reverse()
-        fields.removeAll('lastUpdated', 'dateCreated')
-        exportFields.addAll(fields.collect { GrailsNameUtils.getNaturalName(it).toUpperCase() })
-        if (user) {
-            def data = taskService.exportTasksForUser(user.id)
-            def csvData = FuzzyCSV.toCSV(data, *exportFields)
-            ServletUtil.exportCSV(response, "Tasks-${params.user}.csv", csvData)
-        } else {
-            def data = taskService.exportAllTasks()
-            def csvData = FuzzyCSV.toCSV(data, *exportFields)
-            ServletUtil.exportCSV(response, "Tasks-All.csv", csvData)
-        }
+        super.export DetailerTask
     }
 
     def search(Integer max) {
-        def user = neoSecurityService.currentUser
-        params.max = Math.min(max ?: 50, 100)
-        if (params.term) {
-            redirect(action: 'search', id: params.term)
-            return
-        }
-
-        if (params.remove('ui') == 'map') {
-            redirect(action: 'searchMap', params: params, id: params.term)
-            return
-        }
-
-        def searchTerm = ModelFunctions.getWildCardRegex(params.id as String)
-        def (page, users) = taskService.loadPageDataForUser(user, DetailerTask, params, max, searchTerm)
-        render view: '/task/index', model: [taskInstanceList: page, taskInstanceCount: page.totalElements, users: users]
+        super.search max, DetailerTask, [view: '/task/index']
     }
 
     def searchMap(Integer max) {
-        def user = neoSecurityService.currentUser
-        def searchTerm = ModelFunctions.getWildCardRegex(params.id as String)
-        def (page, users) = taskService.loadPageDataForUser(user, DetailerTask, params, max, searchTerm)
-        def mapData = page.collect { Task t -> taskToJsonMap(t) } as JSON
-        def jsonMapString = mapData.toString(true)
-        respond page.content, view: '/task/map', model: [taskInstanceCount: page.totalElements, users: users, mapData: jsonMapString]
+        super.searchMap max, DetailerTask, [view: '/task/map']
     }
 
     def show() {
-        def id = extractId(params)
-        if (id == -1) {
-            notFound(); return
-        }
-
-        def task = taskService.findTask(id)
-
-        txHelperService.doInTransaction {
-            neo.fetch(task.territoryUser())
-        }
-        render(view: '/task/show', model: [taskInstance: task])
+        super.show view: '/task/show'
     }
 
     def create() {
-        render(view: '/task/create', model: [taskInstance: ModelFunctions.createObj(DetailerTask, params), customers: customerService.listAllCustomers()])
+        render view: '/task/create', model: [taskInstance: ModelFunctions.createObj(DetailerTask, params), customers: customerService.listAllCustomers()]
     }
 
     def save(DetailerTask taskInstance) {
-        if (taskInstance == null) {
-            notFound()
-            return
-        }
-
-        if (taskInstance.hasErrors()) {
-            respond taskInstance.errors, view: 'create'
-            return
-        }
-
-        taskService.saveTask taskInstance
-
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'Task.label', default: 'Task'), taskInstance.id])
-                redirect action: 'show', id: taskInstance.id
-            }
-            '*' { respond taskInstance, [status: CREATED] }
-        }
+        super.save taskInstance
     }
 
-
     def createTaskJson() {
-        handleSafely({
-            def json = request.JSON as Map
-
-            assert json.customerId, 'Please specify Customer Id'
-            def cid = json.customerId as Long
-            def customer = customerService.findCustomer(cid)
-            assert customer, 'Customer Should Exist In DB'
-
-            def dueDate = Date.parse('yyyy-MM-dd', json.dueDate as String)
-
-            def detailerTask = DetailerTask.create(customer, dueDate)
-            taskService.saveTask(detailerTask)
-            return detailerTask.id
-        })
+        super.createTaskJson { Customer c, Date dueDate -> return DetailerTask.create(c, dueDate) }
     }
 
     def edit() {
-        def id = extractId(params)
-
-        if (id == -1) {
-            notFound(); return
-        }
-        def taskInstance = taskService.findOrder(id)
-        render(view: '/task/create', model: [taskInstance: taskInstance])
+        super.edit view: '/task/create'
     }
 
-    @Transactional
-    def delete() {
-
-        def id = extractId(params)
-
-        if (id == -1) {
-            notFound(); return
-        }
-
-        taskService.deleteTask id
-
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Task.label', default: 'Task'), id])
-                redirect action: "index", method: "GET"
-            }
-            '*' { render status: NO_CONTENT }
-        }
-    }
-
-    def handleException(AccessDeniedException ex) {
-        render view: '/login/denied', status: FORBIDDEN
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'Task.label', default: 'Task'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*' { render status: NOT_FOUND }
-        }
-    }
 }
--- grails-app/controllers/com/omnitech/chai/TaskController.groovy
+++ grails-app/controllers/com/omnitech/chai/TaskController.groovy
@@ -1,20 +1,32 @@
 package com.omnitech.chai
 
+import com.omnitech.chai.model.Task
 import com.omnitech.chai.util.ChaiUtils
+import com.omnitech.chai.util.ModelFunctions
+import com.omnitech.chai.util.ReflectFunctions
+import com.omnitech.chai.util.ServletUtil
+import fuzzycsv.FuzzyCSV
 import grails.converters.JSON
+import grails.util.GrailsNameUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.neo4j.support.Neo4jTemplate
 import org.springframework.http.HttpStatus
+import org.springframework.security.access.AccessDeniedException
 
 import java.text.SimpleDateFormat
 
+import static com.omnitech.chai.util.ControllerUtils.customerToJsonMap
+import static com.omnitech.chai.util.ControllerUtils.taskToJsonMap
+import static com.omnitech.chai.util.ModelFunctions.extractId
+import static org.springframework.http.HttpStatus.FORBIDDEN
 import static org.springframework.http.HttpStatus.NOT_FOUND
+import static org.springframework.http.HttpStatus.NO_CONTENT
 
 /**
  * TaskController
  * A controller class handles incoming web requests and performs actions such as redirects, rendering views and so on.
  */
-class TaskController {
+class TaskController extends BaseController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE", updateTaskDate: 'POST']
 
@@ -22,9 +34,175 @@ class TaskController {
     def userService
     def regionService
     def customerService
-    def clusterService
+    def txHelperService
+    def neoSecurityService
     @Autowired
     Neo4jTemplate neo
+
+    protected def index(Integer max, Class<? extends Task> type, Map otherParams) {
+        def user = neoSecurityService.currentUser
+        if (params.remove('ui') == 'map') {
+            redirect(action: 'map', params: params)
+            return
+        }
+        def (page, users) = taskService.loadPageDataForUser(user, type, params, max, null)
+        assert otherParams.view, 'View not specified for this action'
+        render(view: otherParams.view, model: [taskInstanceList: page.content, taskInstanceCount: page.totalElements, users: users])
+    }
+
+    protected def map(Integer max, Class<? extends Task> type, Map otherParams) {
+
+        def user = neoSecurityService.currentUser
+        def (page, users) = taskService.loadPageDataForUser(user, type, params, max, null)
+        def taskData = page.content.collect { Task task ->
+            return taskToJsonMap(task)
+        }
+
+
+        if (params.user) {
+            def userInContext = userService.findUserByName(params.user)
+            //todo use a query for this section to improve performance
+            def customers = customerService.findAllCustomersByUser(userInContext.id, true, [max: 2000])
+            customers.removeAll { c ->
+                page.any { it?.customer?.id == c.id }
+            }
+            def customerData = customers.findAll { it.wkt != null }.collect { customerToJsonMap(it) }
+            taskData.addAll(customerData)
+        }
+
+        def mapData = taskData as JSON
+
+        def jsonMapString = mapData.toString(true)
+        assert otherParams.view, 'View not specified for this action'
+        render(view: otherParams.view, model: [taskInstanceList: page.content, taskInstanceCount: page.totalElements, users: users, mapData: jsonMapString])
+    }
+
+    protected def export(Class<? extends Task> type) {
+        def user = params.user ? userService.findUserByName(params.user) : null
+        def exportFields = ['DISTRICT', 'SUBCOUNTY', 'VILLAGE', 'OUTLET NAME', 'OUTLET TYPE']
+        def fields = ReflectFunctions.findAllBasicFields(type).reverse()
+        fields.removeAll('lastUpdated', 'dateCreated')
+        exportFields.addAll(fields.collect { GrailsNameUtils.getNaturalName(it).toUpperCase() })
+        if (user) {
+            def data = taskService.exportTasksForUser(user.id, type)
+            def csvData = FuzzyCSV.toCSV(data, *exportFields)
+            ServletUtil.exportCSV(response, "Tasks-${params.user}.csv", csvData)
+        } else {
+            def data = taskService.exportAllTasks()
+            def csvData = FuzzyCSV.toCSV(data, *exportFields)
+            ServletUtil.exportCSV(response, "Tasks-All.csv", csvData)
+        }
+    }
+
+    protected def search(Integer max, Class<? extends Task> taskType, Map otherParams) {
+        def user = neoSecurityService.currentUser
+        params.max = Math.min(max ?: 50, 100)
+        if (params.term) {
+            redirect(action: 'search', id: params.term)
+            return
+        }
+
+        if (params.remove('ui') == 'map') {
+            redirect(action: 'searchMap', params: params, id: params.term)
+            return
+        }
+
+        def searchTerm = ModelFunctions.getWildCardRegex(params.id as String)
+        def (page, users) = taskService.loadPageDataForUser(user, taskType, params, max, searchTerm)
+        assert otherParams.view, 'View not specified in action'
+        render view: otherParams.view, model: [taskInstanceList: page, taskInstanceCount: page.totalElements, users: users]
+    }
+
+    protected def searchMap(Integer max, Class<? extends Task> taskType, Map otherParams) {
+        def user = neoSecurityService.currentUser
+        def searchTerm = ModelFunctions.getWildCardRegex(params.id as String)
+        def (page, users) = taskService.loadPageDataForUser(user, taskType, params, max, searchTerm)
+        def mapData = page.collect { Task t -> taskToJsonMap(t) } as JSON
+        def jsonMapString = mapData.toString(true)
+        respond page.content, view: '/task/map', model: [taskInstanceCount: page.totalElements, users: users, mapData: jsonMapString]
+    }
+
+    protected def show(Map otherParams) {
+        def id = extractId(params)
+        if (id == -1) {
+            notFound(); return
+        }
+
+        def task = taskService.findTask(id)
+
+        txHelperService.doInTransaction {
+            neo.fetch(task.territoryUser())
+        }
+        assert otherParams.view, 'View not specified in action'
+        render view: otherParams.view, model: [taskInstance: task]
+    }
+
+    protected def save(Task taskInstance) {
+        if (taskInstance == null) {
+            notFound()
+            return
+        }
+
+        if (taskInstance.hasErrors()) {
+            respond taskInstance.errors, view: 'create'
+            return
+        }
+
+        taskService.saveTask taskInstance
+
+        flash.message = message(code: 'default.created.message', args: [message(code: 'Task.label', default: taskInstance.getClass().simpleName), taskInstance.id])
+        redirect action: 'show', id: taskInstance.id
+
+    }
+
+    protected def edit(Map otherParams) {
+        def id = extractId(params)
+
+        if (id == -1) {
+            notFound(); return
+        }
+        def taskInstance = taskService.findTask(id)
+
+        assert otherParams.view, 'View not specified in action'
+        render view: otherParams.view, model: [taskInstance: taskInstance]
+    }
+
+    def delete() {
+
+        def id = extractId(params)
+
+        if (id == -1) {
+            notFound(); return
+        }
+
+        taskService.deleteTask id
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Task.label', default: 'Task'), id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NO_CONTENT }
+        }
+    }
+
+
+    def createTaskJson(Closure<Task> create) {
+        handleSafely {
+            def json = request.JSON as Map
+
+            assert json.customerId, 'Please specify Customer Id'
+            def cid = json.customerId as Long
+            def customer = customerService.findCustomer(cid)
+            assert customer, 'Customer Should Exist In DB'
+
+            def dueDate = Date.parse('yyyy-MM-dd', json.dueDate as String)
+
+            def detailerTask = create(customer, dueDate)
+            taskService.saveTask(detailerTask)
+            return detailerTask.id
+        }
+    }
 
 
     def updateTaskDate() {
@@ -41,6 +219,10 @@ class TaskController {
         }
 
 
+    }
+
+    def handleException(AccessDeniedException ex) {
+        render view: '/login/denied', status: FORBIDDEN
     }
 
     def cluster() {
