2718b590b74c8c5283cf1c22dd43b17d467c6ef1
kayr <noryak1234@gmail.com>
2015-02-20 01:03:34 +0300

    some finetuning of clusters

--- grails-app/services/com/omnitech/chai/crm/ClusterService.groovy
+++ grails-app/services/com/omnitech/chai/crm/ClusterService.groovy
@@ -1,6 +1,7 @@
 package com.omnitech.chai.crm
 
 import com.omnitech.chai.model.*
+import com.omnitech.chai.util.ChaiUtils
 import org.apache.commons.math3.ml.clustering.CentroidCluster
 import org.apache.commons.math3.ml.clustering.Clusterable
 import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer
@@ -26,13 +27,18 @@ class ClusterService {
     static int NUMBER_OF_USERS = 1
 
 
-    public double distanceBetweenPoints(LatLng pointA, LatLng pointB) {
+    static public double distanceBetweenPoints(LatLng pointA, LatLng pointB) {
+        distanceBetweenPoints([pointA.lat, pointA.lng] as double[], [pointA.lat, pointB.lng] as double[])
+    }
+
+
+    static public double distanceBetweenPoints(double[] pointA, double[] pointB) {
         // Setup the inputs to the formula
         double R = 6371009d; // average radius of the earth in metres
-        double dLat = Math.toRadians(pointB.getLat() - pointA.getLat());
-        double dLng = Math.toRadians(pointB.getLng() - pointA.getLng());
-        double latA = Math.toRadians(pointA.getLat());
-        double latB = Math.toRadians(pointB.getLat());
+        double dLng = Math.toRadians(pointB[0] - pointA[0]);
+        double dLat = Math.toRadians(pointB[1] - pointA[1]);
+        double latA = Math.toRadians(pointA[0]);
+        double latB = Math.toRadians(pointB[0]);
 
         // The actual haversine formula. a and c are well known value names in the formula.
         double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
@@ -78,11 +84,14 @@ class ClusterService {
 
             nextDate = getNextWorkDay(workDays, nextDate)
 
+
             entry.getPoints().each { locatableTask ->
+                println("Setting next date: $nextDate - [$locatableTask.task.description]")
                 locatableTask.task.setDueDate(nextDate)
             }
 
             nextDate = ++nextDate
+
         }
     }
 
@@ -96,7 +105,15 @@ class ClusterService {
             return null
         } as List<LocatableTask>
 
-        def clusters = getClusters(locatableTasks, tasksPerDay, 1)
+        def (List<CentroidCluster<LocatableTask>> clusters, List other) = getClusters2(locatableTasks, tasksPerDay, 20, 0.3f, true)
+
+
+        clusters.sort { CentroidCluster<LocatableTask> a, CentroidCluster<LocatableTask> b ->
+            def dis = distanceBetweenPoints(
+                    [getLat: { a.center.point[0] }, getLng: { a.center.point[1] }] as LatLng,
+                    [getLat: { b.center.point[0] }, getLng: { b.center.point[1] }] as LatLng)
+            return dis
+        }
 
         assignDueDateToClusters(clusters, startDate, allowedDays)
         return clusters
@@ -109,6 +126,59 @@ class ClusterService {
         def clusterer = new KMeansPlusPlusClusterer<LocatableTask>(numberOfClusters as int, 10000);
         List<CentroidCluster<LocatableTask>> clusters = clusterer.cluster(locatableTasks);
         return clusters
+    }
+    //20 is a magic number to reduce the number of clusters
+    static List getClusters2(List<LocatableTask> locatableTasks, int tasksPerDay, int _magicTasksPerDay, float _percOverheadTaskPerDay, boolean processMissedPoint = false) {
+        def taskSize = locatableTasks.size()
+
+        def numberOfClusters = calculateBestNumberOfCluster(taskSize, _magicTasksPerDay)
+        def clusterer = new KMeansPlusPlusClusterer<LocatableTask>(numberOfClusters as int, 10000);
+        List<CentroidCluster<LocatableTask>> clusters = clusterer.cluster(locatableTasks);
+
+        if (clusters.size() == 1) return [clusters, []]
+
+
+        def smallClusters = []
+        List<LocatableTask> missedPoints = []
+
+        def maximumNumberOfTask = (tasksPerDay + (tasksPerDay * _percOverheadTaskPerDay)).toInteger()
+        def minimumSize = (tasksPerDay - (tasksPerDay * 0.2)).toInteger()
+        for (cluster in clusters) {
+            if (cluster.points.size() > maximumNumberOfTask) {
+                def (subClusters, mp) = getClusters2(cluster.points, tasksPerDay, maximumNumberOfTask, 0.3f)
+                missedPoints.addAll(mp)
+                smallClusters.addAll(subClusters)
+            } else if (cluster.points.size() < minimumSize) {
+                missedPoints.addAll(cluster.points)
+            } else {
+                smallClusters << cluster
+            }
+        }
+
+        if (processMissedPoint && missedPoints) {
+            def (smallSubCluster, List<LocatableTask> mp) = getClusters2(missedPoints, tasksPerDay, maximumNumberOfTask, 0.3f)
+            smallClusters.addAll(smallSubCluster)
+            if (mp) {
+                //add remaining tasks to closest clusters
+                mp.each { t ->
+                    def closestCluster = smallClusters.min { CentroidCluster a ->
+
+                        def distance = distanceBetweenPoints([t.lat, t.lng] as double[], a.center.point)
+                        log.trace("t[${t.task.description}] ->[${a.points[0].task.description}] = $distance")
+                        return distance
+                    }
+                    log.trace("t[${t.task.description}] is clossest to : [${closestCluster.points[0].task.description}]")
+                    closestCluster.addPoint(t)
+                }
+            }
+        }
+
+        return [smallClusters, missedPoints]
+    }
+
+    static int calculateBestNumberOfCluster(int taskSize, int absoluteTaskPerDay) {
+        return ChaiUtils.roundUpward(taskSize, absoluteTaskPerDay) / absoluteTaskPerDay
+
     }
 
 
@@ -135,7 +205,7 @@ class ClusterService {
 
 }
 
-class LocatableTask implements Clusterable {
+class LocatableTask implements Clusterable, LatLng {
 
     Task task
 
@@ -144,5 +214,15 @@ class LocatableTask implements Clusterable {
         def lat = task.customer.lat as double
         def lng = task.customer.lng as double
         return [lat, lng] as double[]
+    }
+
+    @Override
+    Double getLat() {
+        return task.customer.lat as double
+    }
+
+    @Override
+    Double getLng() {
+        return task.customer.lng as double
     }
 }
