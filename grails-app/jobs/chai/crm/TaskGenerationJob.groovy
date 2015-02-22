2158ee5b22b958f7248eb6ce988fdc72d780fa42
kayr <noryak1234@gmail.com>
2015-02-22 20:37:05 +0300

    Task generation is every day at midnight

--- grails-app/jobs/chai/crm/TaskGenerationJob.groovy
+++ grails-app/jobs/chai/crm/TaskGenerationJob.groovy
@@ -11,7 +11,9 @@ class TaskGenerationJob {
 
     static triggers = {
         //execute every sunday at midnight
-        cron name: 'taskGenerator', startDelay: 10000, cronExpression: '0 0 0 ? * 1'
+//        cron name: 'taskGenerator', startDelay: 10000, cronExpression: '0 0 0 ? * 1'
+        //every day at 12am
+        cron name: 'taskGenerator', startDelay: 10000, cronExpression: '0 0 0 1/1 * ? *'
 //        cron name: 'taskGenerator', startDelay: 10000, cronExpression: '0 0/1 * 1/1 * ? *'
     }
 
@@ -22,7 +24,7 @@ class TaskGenerationJob {
 
         segments.each { it.numberOfTasks = 2000 }
 
-        taskService.generateTasks(territories,segments,(new Date() + 1095),[Calendar.MONDAY],10,SalesCall,false)
+        taskService.generateTasks(territories, segments, (new Date() + 1095), [Calendar.MONDAY], 10, SalesCall, false)
         println("######################\nFinished Generating tasks\n##################################")
     }
 }
