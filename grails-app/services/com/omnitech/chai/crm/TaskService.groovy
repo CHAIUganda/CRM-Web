9654bf19c2ce8a6e9f0e81a26862b5c38039b8a1
kayr <noryak1234@gmail.com>
2015-03-16 14:04:44 +0300

    put back aggrations in export

--- grails-app/services/com/omnitech/chai/crm/TaskService.groovy
+++ grails-app/services/com/omnitech/chai/crm/TaskService.groovy
@@ -256,8 +256,8 @@ class TaskService {
             def queries = products.collect { p ->
                 def productName = p.name.toUpperCase() + '-' + (p.unitOfMeasure ?: '')
                 returnFields.add(productName)
-//                "sum (case when id(p) = $p.id then li.quantity else null end) as `${productName}`"
-                "case when id(p) = $p.id then li.quantity else null end as `${productName}`"
+               return  "sum (case when id(p) = $p.id then li.quantity else null end) as `${productName}`"
+//                "case when id(p) = $p.id then li.quantity else null end as `${productName}`"
             }
             def joinedExpressions = queries.join(',')
             if (joinedExpressions)
