8e8928ec6fe9d7a3aa1cb8a949da925b311bb410
kayr <noryak1234@gmail.com>
2015-03-23 14:50:55 +0300

    simple script update

--- upload-new-warfile.bat
+++ upload-new-warfile.bat
@@ -1,12 +1,35 @@
 rem grails war
+
+IF NOT EXIST "target\chai-crm-0.1.war" GOTO WAR_FILE_NOT_EXISTS
+IF EXIST "G:\professional\binaryrepos\chai-crm-0.1-old\" GOTO DELETE_OLD
+
 copy target\chai-crm-0.1.war G:\professional\binaryrepos\chai-crm-0.1.war /y
+
 G:
+
 cd G:\professional\binaryrepos\
+
 move chai-crm-0.1 chai-crm-0.1-old
-rem winrar x chai-crm-0.1.war chai-crm-0.1\
+
 unzip -o chai-crm-0.1.war -d chai-crm-0.1\
+
 xcopy chai-crm-0.1-old\.git chai-crm-0.1\.git\ /s /y
+
 cd chai-crm-0.1\
+
 git add -A
+
 git gui
+
 start G:\professional\binaryrepos\
+
+
+:DELETE_OLD
+echo "chai-crm-old EXISTS please delete it !!"
+GOTO END
+
+:WAR_FILE_NOT_EXISTS
+echo "war file not EXISTS please build one!!"
+GOTO END
+
+:END
